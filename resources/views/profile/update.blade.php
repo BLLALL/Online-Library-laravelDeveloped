e925ce94a138ea74dd2ee29f97cb1a3b2df97faa
belal <bamer8353@gmail.com>
2024-06-25 18:34:56 +0200

    Revert "Create laravel.yml"
    This reverts commit 107ce3cd

--- resources/views/profile/update.blade.php
+++ resources/views/profile/update.blade.php
@@ -48,7 +48,5 @@
             integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
             crossorigin="anonymous"></script>
     <script src="/JS/newwwww.js"></script>
-    </body>
 
-    </html>
 </x-layout>
